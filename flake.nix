1b98c08f40e2e07d788eb9a87ef9c5eda3e404bd
hugosenari <hugosenari@gmail.com>
2022-07-21 22:17:11 -0300

    chore: re index ago

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''Time ago in words in Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."ago-master".dir   = "master";
+  inputs."ago-master".owner = "nim-nix-pkgs";
+  inputs."ago-master".ref   = "master";
+  inputs."ago-master".repo  = "ago";
+  inputs."ago-master".type  = "github";
+  inputs."ago-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."ago-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
